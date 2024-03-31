5f3b0d1edd93a7a2d4621e34a171dd13df6cc7ba
Hashem Wannous <hashemwnoos@gmail.com>
2024-03-31 11:40:06 +0300

    Redux Toolkit!

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { counterSlice } from "../../store/features/Counter/CounterSlice";
 
 import './Home.scss';
 
 import Page from "../../components/Page/Page";
 
 export default function Home(): React.ReactElement {
+    const { value } = useSelector(counterSlice.selectSlice);
+    const Dispatch = useDispatch();
+
     return (
         <Page id="home-page">
             <h1>Welcome Back!</h1>
+            <section>
+                <p>{value}</p>
+                <button onClick={_ => Dispatch(counterSlice.actions.increment())}>Increment</button>
+            </section>
         </Page>
     );
 }
